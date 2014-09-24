f2f4bfbb9ca2bd00c3ef8a9a9f916fe8d54f873d
Zan Loy <zan.loy@gmail.com>
2014-09-24 12:27:17 -0400

    a

--- manifests/os/redhat.pp
+++ manifests/os/redhat.pp
@@ -1,9 +1,9 @@
 class ldap::os::redhat {
 
-  file { '/etc/ldap.conf':
-    ensure  => link,
-    target  => "${ldap::params::prefix}/${ldap::params::config}",
-    require => File["${ldap::params::prefix}/${ldap::params::config}"],
-  }
+  #file { '/etc/ldap.conf':
+  #  ensure  => link,
+  #  target  => "${ldap::params::prefix}/${ldap::params::config}",
+  #  require => File["${ldap::params::prefix}/${ldap::params::config}"],
+  #}
 
 }
