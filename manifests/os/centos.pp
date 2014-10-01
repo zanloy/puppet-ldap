4b1d99663953b5aacee40c447a33d6521f51aa69
Zan Loy <zan.loy@gmail.com>
2014-10-01 13:29:14 -0400

    added centos support

--- manifests/os/centos.pp
+++ manifests/os/centos.pp
@@ -1,1 +1,9 @@
-redhat.pp
+class ldap::os::centos {
+
+  file { '/etc/ldap.conf':
+    ensure  => link,
+    target  => "${ldap::params::prefix}/${ldap::params::config}",
+    require => File["${ldap::params::prefix}/${ldap::params::config}"],
+  }
+
+}
